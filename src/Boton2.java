4e4404273446e4fffa1a38f9a5b46dd42f6ea34a
Antonio Casares <acb2610@gmail.com>
2015-05-19 10:35:53 +0200

    Buscaminas   Antonio

---
+++ src/Boton2.java
@@ -0,0 +1,21 @@
+
+
+
+import Principal.*;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Antonio Casares
+
+ */
+public class Boton2 extends JButton{
+   int bomba = 0;
+   int x,y;
+   int numeroMinasAlrededor = 1;
+   //constructor básico para iniciar la x y la y 
+   public Boton2(int _x, int _y){
+       x = _x;
+       y = _y;
+   }
+}
